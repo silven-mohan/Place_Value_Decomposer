ce48d0bcb6991d07310800273a5183551470055f
Silven Mohan <silven667@gmail.com>
2025-07-06 18:43:05 +0530

    Initial Commit

---
+++ PlaceValue.c
@@ -0,0 +1,29 @@
+/** Place_Value **/
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+
+int main()
+{
+	int num, i, len=0, digit, temp;
+	printf("Enter any number:");
+	scanf("%d", &num);
+	temp=num;
+	while(temp!=0)
+	{
+		len++;
+		temp/=10;
+	}
+	temp=num;
+	for(i=1;i<=len;i++)
+	{
+		digit=temp/((int)pow(10, len-i));
+		printf("%d", digit*((int)pow(10, len-i)));
+		temp=temp%((int)pow(10, len-i));
+		if(i!=(len))
+			printf(" + ");
+	}
+	
+	return 0;
+}
